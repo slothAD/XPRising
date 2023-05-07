3b2eb9fd2b5aa8e7a8667b4ab1f6a9b2751ffdfb
Jason Williams <jason@williamsemail.us>
2023-05-07 12:26:18 -0400

    Added dynamic configurable classes. Added load database method so play testing/balancing the stats will be easier.

---
+++ Commands/Load.cs
@@ -0,0 +1,35 @@
+﻿using ProjectM;
+using RPGMods.Utils;
+using System.Text.RegularExpressions;
+
+namespace RPGMods.Commands
+{
+    [Command("load", Usage = "load", Description = "Force the server to load RPGMods DB from a json file.")]
+    public static class Load
+    {
+        public static void Initialize(Context ctx)
+        {
+            var args = ctx.Args;
+            string name = "Manual Save";
+            if (args.Length >= 1)
+            {
+                name = string.Join(' ', ctx.Args);
+                if (name.Length > 50)
+                {
+                    Output.CustomErrorMessage(ctx, "Name is too long!");
+                    return;
+                }
+                if (Regex.IsMatch(name, @"[^a-zA-Z0-9\x20]"))
+                {
+                    Output.CustomErrorMessage(ctx, "Name can only contain alphanumeric & space!");
+                    return;
+                }
+            }
+
+            Output.SendSystemMessage(ctx, $"Loading data....");
+            AutoSaveSystem.LoadDatabase();
+            //Plugin.Server.GetExistingSystem<TriggerPersistenceSaveSystem>().TriggerSave(SaveReason.ManualSave, name);
+            Output.SendSystemMessage(ctx, $"Data load complete.");
+        }
+    }
+}
