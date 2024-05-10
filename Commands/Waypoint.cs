50309d82120e38314dc746cb1c1ae98eb209d032
Anthony Mosca <anthony.mosca@gmail.com>
2024-05-10 12:41:34 +0930

    Minor waypoint command tweaks

--- Commands/Waypoint.cs
+++ Commands/Waypoint.cs
@@ -1,5 +1,6 @@
 using OpenRPG.Utils;
 using System;
+using System.Linq;
 using OpenRPG.Models;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -18,12 +19,7 @@ namespace OpenRPG.Commands {
                 return;
             }
 
-            if (Database.waypoints.TryGetValue(waypoint, out var wpData)) {
-                Helper.TeleportTo(ctx, wpData.ToFloat3());
-                return;
-            }
-
-            if (Database.waypoints.TryGetValue(waypoint + "_" + steamID, out wpData)) {
+            if (Database.waypoints.TryGetValue(waypoint + "_" + steamID, out var wpData)) {
                 if (WaypointLimit <= 0 && !ctx.IsAdmin) {
                     ctx.Reply("Personal Waypoints are forbidden to you.");
                     if(Database.waypoints.Remove(waypoint + "_" + steamID)) {
@@ -34,6 +30,11 @@ namespace OpenRPG.Commands {
                 Helper.TeleportTo(ctx, wpData.ToFloat3());
                 return;
             }
+
+            if (Database.waypoints.TryGetValue(waypoint, out wpData)) {
+                Helper.TeleportTo(ctx, wpData.ToFloat3());
+                return;
+            }
             ctx.Reply("Waypoint not found.");
         }
 
@@ -41,54 +42,47 @@ namespace OpenRPG.Commands {
         [Command("set", "s", "<waypoint name>", "Creates the specified personal waypoint", adminOnly: false)]
         public static void SetWaypoint(ChatCommandContext ctx, string name) {
             if(WaypointLimit <= 0 && !ctx.IsAdmin) {
-                ctx.Reply("You may not create waypoints.");
-                return;
+                throw ctx.Error("You may not create waypoints.");
             }
             var steamID = ctx.Event.User.PlatformId;
-            if (Database.waypoints.TryGetValue(name, out _)) {
-                ctx.Reply($"A global waypoint with the \"{name}\" name existed. Please rename your waypoint.");
-                return;
+
+            var waypointCount = Database.waypoints.Keys.Count(wpName => wpName.EndsWith($"{steamID}"));
+            if (waypointCount >= WaypointLimit && !ctx.IsAdmin) {
+                throw ctx.Error("You already have reached your total waypoint limit.");
             }
-            if (Database.waypoints_owned.TryGetValue(steamID, out var total) && !ctx.IsAdmin) {
-                if (total >= WaypointLimit) {
-                    ctx.Reply("You already have reached your total waypoint limit.");
-                    return;
-                }
-            }
-            string waypointName = name + "_" + steamID;
+            var waypointName = name + "_" + steamID;
             if (Database.waypoints.TryGetValue(name, out _)) {
                 ctx.Reply($"You already have a waypoint with the same name.");
                 return;
             }
             var location = Plugin.Server.EntityManager.GetComponentData<LocalToWorld>(ctx.Event.SenderCharacterEntity).Position;
-            AddWaypoint(steamID, location, waypointName, name, false);
+            Database.waypoints[waypointName] = new WaypointData(location);
             ctx.Reply("Successfully added Waypoint.");
         }
 
         [Command("set global", "sg", "<waypoint name>", "Creates the specified global waypoint", adminOnly: true)]
         public static void SetGlobalWaypoint(ChatCommandContext ctx, string name) {
-            var steamID = ctx.Event.User.PlatformId;
             var location = Plugin.Server.EntityManager.GetComponentData<LocalToWorld>(ctx.Event.SenderCharacterEntity).Position;
-            AddWaypoint(steamID, location, name, name, true);
+            Database.waypoints[name] = new WaypointData(location);
             ctx.Reply("Successfully added Waypoint.");
         }
 
         [Command("remove global", "rg", "<waypoint name>", "Removes the specified global waypoint", adminOnly: true)]
         public static void RemoveGlobalWaypoint(ChatCommandContext ctx, string name) {
-            var steamID = ctx.Event.User.PlatformId;
-            RemoveWaypoint(steamID, name, true);
+            Database.waypoints.Remove(name);
             ctx.Reply("Successfully removed Waypoint.");
         }
 
         [Command("remove", "r", "<waypoint name>", "Removes the specified personal waypoint", adminOnly: false)]
         public static void RemoveWaypoint(ChatCommandContext ctx, string name) {
             var steamID = ctx.Event.User.PlatformId;
-            string waypointName = name + "_" + steamID;
+            var waypointName = name + "_" + steamID;
             if (!Database.waypoints.TryGetValue(waypointName, out _)) {
                 ctx.Reply($"You do not have any waypoint with this name.");
                 return;
             }
-            RemoveWaypoint(steamID, waypointName, false);
+
+            Database.waypoints.Remove(name);
             ctx.Reply("Successfully removed Waypoint.");
         }
 
@@ -130,24 +124,6 @@ namespace OpenRPG.Commands {
             }
             if (totalWaypoints == 0) ctx.Reply("No waypoint available.");
 
-        }
-
-        private static void AddWaypoint(ulong owner, float3 location, string name, string trueName, bool isGlobal) {
-            if (isGlobal) Database.waypoints[trueName] = new WaypointData(location);
-            else Database.waypoints[name] = new WaypointData(location);
-            if (!isGlobal && Database.waypoints_owned.TryGetValue(owner, out var total)) {
-                Database.waypoints_owned[owner] = total + 1;
-            } else if(!isGlobal) Database.waypoints_owned[owner] = 1;
-        }
-
-        private static void RemoveWaypoint(ulong owner, string name, bool global) {
-            if (global) {
-                Database.waypoints.Remove(name);
-            } else {
-                Database.waypoints_owned[owner] -= 1;
-                if (Database.waypoints_owned[owner] < 0) Database.waypoints_owned[owner] = 0;
-                Database.waypoints.Remove(name);
-            }
         }
     }
 }
